96cb14191bdb43384fa79d8290335f03b34f0e9b
j1t1b <zhangfred11@alumni.ubc.ca>
2020-02-15 10:50:50 -0800

    actual initial commit?

---
+++ Assets/Scripts/InteractWithThrowable.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractWithThrowable : MonoBehaviour
+{
+    public GameObject giant;
+    private Throwables tbl;
+    private Collider Blah;
+    private bool zowie;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnCollisionStay(Collision collision){
+        Throwables temp = collision.collider.GetComponent<Throwables>();
+
+        if (temp != null){
+            temp.enablesInteract();
+        }
+    }
+
+}
---
+++ Assets/Scripts/MovementTesting.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementTesting : MonoBehaviour
+{
+    private Collider body;
+    public float speed;
+    // Start is clled before the first frame update
+    void Start()
+    {
+        body = GetComponent<CapsuleCollider>();   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey("w")){
+            body.transform.position += Time.deltaTime * speed * new Vector3(-1,0,0);
+
+        }if (Input.GetKey("s")){
+            body.transform.position += Time.deltaTime * speed * new Vector3(1,0,0);
+
+        }if (Input.GetKey("a")){
+            body.transform.position += Time.deltaTime * speed * new Vector3(0,0,-1);
+
+        }if (Input.GetKey("d")){
+            body.transform.position += Time.deltaTime * speed * new Vector3(0,0,1);
+
+        }
+    }
+}
