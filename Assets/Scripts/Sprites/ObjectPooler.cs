f3da35e38e0c81fb8449b1e7a528d5e32438b7fd
j1t1b <zhangfred11@alumni.ubc.ca>
2020-02-15 22:00:56 -0800

    added object pooled

---
+++ Assets/Scripts/Sprites/ObjectPooler.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectPooler : MonoBehaviour
+{
+    public Dictionary<string, Queue<GameObject>> objectDictionary;
+
+    [System.Serializable]
+    public class Pool{
+        public string tag;
+        public GameObject prefab;
+        public int size;
+    }
+
+    public static ObjectPooler Instance;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    public List<Pool> pools;
+    void Start()
+    {
+        objectDictionary = new Dictionary<string, Queue<GameObject>>();
+
+        foreach(Pool pool in pools){
+            Queue<GameObject> objectPool = new Queue<GameObject>();
+
+            for (int i = 0; i < pool.size; i++){
+
+                GameObject obj = Instantiate(pool.prefab);
+                obj.SetActive(false);
+                objectPool.Enqueue(obj);
+
+            }
+
+            objectDictionary.Add(pool.tag, objectPool);
+        }
+
+        
+    }
+
+    public GameObject SpawnFromPool(string tag, Vector3 position, Quaternion rotation){
+        if (!objectDictionary.ContainsKey(tag)){
+            Debug.Log("you fked");
+            return null;
+        }
+
+        GameObject objectToSpawn = objectDictionary[tag].Dequeue();
+
+        objectToSpawn.SetActive(true);
+        objectToSpawn.transform.position = position;
+        objectToSpawn.transform.rotation = rotation;
+
+        IPooledObject pooledObj = objectToSpawn.GetComponent<IPooledObject>();
+
+        if (pooledObj != null){
+            pooledObj.OnObjectSpawn();
+        }
+
+        objectDictionary[tag].Enqueue(objectToSpawn);
+
+        return objectToSpawn;
+    }
+
+}
