e1222d54eb60072378e3464e42376b73824d9d80
T.J. Wu <tjwu24@gmail.com>
2020-02-16 15:31:37 -0800

    set pool in, pool init for end condition

--- Assets/Scripts/Pool.cs
+++ Assets/Scripts/Pool.cs
@@ -4,22 +4,28 @@ using UnityEngine;
 
 public class Pool : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private int totalNpc;
+    private int currNpc;
     void Start()
     {
-        
+        totalNpc = GameObject.FindGameObjectsWithTag("NPC").Length;
+        currNpc = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (currNpc == totalNpc)
+        {
+            // TODO
+        }
     }
 
     void OnTriggerEnter(Collider collider)
     {
         if (collider.gameObject.tag == "NPC")
         {
+            currNpc++;
             collider.gameObject.GetComponent<NpcAnimation>().OnWon();
             collider.gameObject.GetComponent<NpcMovement>().OnWon();
             collider.gameObject.GetComponent<NpcAttributes>().OnWon();
