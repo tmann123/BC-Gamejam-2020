17f46e5d4184402e98ee997265f240f6f18c878a
Tyler Mann <43198653+tmann123@users.noreply.github.com>
2020-02-15 23:41:43 -0800

    Added title and pause menu

---
+++ Assets/Scripts/Menu/PauseMenu.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    private bool gameIsPaused = false;
+
+    // Resume game
+    public void Resume() {
+        Time.timeScale = 1f;
+        gameIsPaused = false;
+    }
+
+    // Pause game
+    public void Pause() {
+        Time.timeScale = 0f;
+        gameIsPaused = true;
+    }
+
+    // Go back to the main menu
+    public void Quit() {
+        SceneManager.LoadSceneAsync("MainMenu");
+    }
+}
